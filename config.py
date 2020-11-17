625005a779a0c7b579d652cd83f42dd70086e0bc
Irek Valiullin <irekvaliullin2016@yandex.ru>
2020-11-17 17:13:43 +0300

    15

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Configuration:
-    DEBUG = True
+   # DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///luxury_auto.db'
     UPLOAD_FOLDER = '/path/to/the/uploads'
